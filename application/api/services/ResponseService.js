e3d4bf2a219174c5eef35d05aabf0146ab82f26f
Chuka Ofili <ofili@microsmart.tk>
2017-03-24 10:58:18 +0100

    Updated response service with new syntax format

--- application/api/services/ResponseService.js
+++ application/api/services/ResponseService.js
@@ -4,15 +4,13 @@
 module.exports = {
     json: function(status, res, message, data, meta) {
         var response = {
-            response: {
                 message: message
-            }
         };
         if (typeof data !== 'undefined') {
-            response.response.data = data;
+            response.data = data;
         }
         if (typeof meta !== 'undefined') {
-            response.response.meta = meta;
+            response.meta = meta;
         }
         return res.status(status).json(response);
     }
